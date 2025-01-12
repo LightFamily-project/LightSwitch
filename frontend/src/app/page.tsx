8e4a7d211bea3404e603dce23be35e31372b4672
HakSooKim <haksoo@glassdomeinc.com>
2025-01-12 19:37:50 +0900

    feat: add radix ui dependency

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
 export default function Home() {
-  return <div>Light Switch</div>;
+  return (
+    <div>
+      Light Switch
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
+    </div>
+  );
 }
