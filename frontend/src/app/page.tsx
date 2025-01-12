49e23e466128ce14d014f0e6c37003d1252eef93
HakSooKim <haksoo@glassdomeinc.com>
2025-01-13 00:53:09 +0900

    feat: add shadcn setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div>Light Switch</div>;
+  return (
+    <div>
+      <h1>Light Switch</h1>
+      <Button size="lg">Click me</Button>
+    </div>
+  );
 }
