0c5acd1fb2c2fd8eaf341e24ddd578f91d9430a6
HakSooKim <haksoo@glassdomeinc.com>
2025-01-12 23:19:48 +0900

    style: run lint

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,21 +1,21 @@
-import { Theme, ThemePanel } from "@radix-ui/themes";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Theme } from '@radix-ui/themes';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Light Switch",
-  description: "A lightweight, open-source, self-hosted feature toggle tool.",
+  title: 'Light Switch',
+  description: 'A lightweight, open-source, self-hosted feature toggle tool.',
 };
 
 export default function RootLayout({
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Button } from "@radix-ui/themes";
+import { Flex, Text, Button } from '@radix-ui/themes';
 
 export default function Home() {
   return (
@@ -6,7 +6,7 @@ export default function Home() {
       Light Switch
       <Flex direction="column" gap="2">
         <Text>Hello from Radix Themes :)</Text>
-        <Button>Let's go</Button>
+        <Button>Let&apos;s go</Button>
       </Flex>
     </div>
   );
