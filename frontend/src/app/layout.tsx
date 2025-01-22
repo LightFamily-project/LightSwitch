05c6ce1be324fea940e0360e44a1e09196b1b7ef
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2025-01-22 21:39:03 +0900

    feat: Implement sideBar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { AppSidebar } from '@/components/ui/app-sidebar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppSidebar />
+        <main>
+          <div className="flex min-h-screen items-center justify-center bg-gray-100">
+            <div className="w-full">{children}</div>
+          </div>
+        </main>
       </body>
     </html>
   );
