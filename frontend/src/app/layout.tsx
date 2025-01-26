15da0b2ad455ec988fe36f946f6717d7c72f2396
HakSooKim <haksoo@glassdomeinc.com>
2025-01-26 21:15:35 +0900

    refactor: improve sidebar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,47 +1,30 @@
-// css
-import './globals.css';
-import '@/app/styles.css';
 import type { Metadata } from 'next';
-// font
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-// components
-import { AppSidebar } from '@/components/ui/app-sidebar';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { LayoutContent } from './LayoutContent';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+import { ThemeProvider } from '@/components/theme-provider';
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Light Switch',
   description: 'A lightweight, open-source, self-hosted feature toggle tool.',
 };
 
-export default function RootLayout({ children }: Readonly) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <SidebarProvider>
-          <AppSidebar />
-          <main className="flex-1 bg-[#09090B]">
-            <SidebarTrigger
-              className="sidebarController"
-              style={{ color: 'white' }}
-            />
-            <div className="flex min-h-screen items-center justify-center">
-              <div className="w-full">{children}</div>
-            </div>
-          </main>
-        </SidebarProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
+          <LayoutContent>{children}</LayoutContent>
+        </ThemeProvider>
       </body>
     </html>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white">Light Switch</h1>
+      <h1>Light Switch</h1>
     </div>
   );
 }
