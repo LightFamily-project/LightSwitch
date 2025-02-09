5438af6e6b5bc8053fd4a6368374dfa34e3ab314
Nayeon Kim <inourbubble2@gmail.com>
2025-02-09 18:05:26 +0900

    [chore] : Move web -> infrastructure.swagger

--- backend/src/main/kotlin/com/lightswitch/infrastructure/swagger/SwaggerConfig.kt
+++ backend/src/main/kotlin/com/lightswitch/infrastructure/swagger/SwaggerConfig.kt
@@ -1,4 +1,4 @@
-package com.lightswitch.web.swagger
+package com.lightswitch.infrastructure.swagger
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.security.SecurityScheme
