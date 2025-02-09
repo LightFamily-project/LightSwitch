8068bc3626ac9c48f8338283188bb8b6202318e0
Nayeon Kim <inourbubble2@gmail.com>
2025-02-09 20:03:10 +0900

    feat: Add CreateFeatureFlagRequest

---
+++ backend/src/main/kotlin/com/lightswitch/presentation/model/CreateFeatureFlagRequest.kt
@@ -0,0 +1,21 @@
+package com.lightswitch.presentation.model
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+
+data class CreateFeatureFlagRequest(
+    @field:NotBlank(message = "Key is required.")
+    val key: String,
+    @field:NotNull(message = "Status is required.")
+    val status: Boolean,
+    @field:NotNull(message = "Type is required.")
+    val type: String,
+    @field:NotEmpty(message = "Default value is required.")
+    val defaultValue: Pair<String, Any>,
+    @field:NotBlank(message = "Description is required.")
+    val description: String,
+    val variants: List<Pair<String, Any>>? = null,
+    @field:NotBlank(message = "CreatedBy is required.")
+    val createdBy: String,
+)
