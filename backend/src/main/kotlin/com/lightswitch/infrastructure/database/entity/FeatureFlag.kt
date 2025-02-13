4f165951ba6763cb765a1940c0e20f88a10bf3e9
Nayeon Kim <inourbubble2@gmail.com>
2025-02-13 16:07:09 +0900

    fix: Refactor FeatureFlag entity
    - Replaced `defaultConditionId` with a `@OneToOne` relationship to `Condition`
    - Add a `description` field for additional metadata

--- backend/src/main/kotlin/com/lightswitch/infrastructure/database/entity/FeatureFlag.kt
+++ backend/src/main/kotlin/com/lightswitch/infrastructure/database/entity/FeatureFlag.kt
@@ -1,12 +1,15 @@
 package com.lightswitch.infrastructure.database.entity
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.LastModifiedBy
 
@@ -15,9 +18,12 @@ class FeatureFlag(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
-    var defaultConditionId: Long? = null,
+    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL], optional = true)
+    @JoinColumn(name = "default_condition_id", referencedColumnName = "id", nullable = true)
+    var defaultCondition: Condition? = null,
     @Column(nullable = false)
     val name: String,
+    val description: String,
     @Column(nullable = false)
     val type: String,
     @Column(nullable = false)
