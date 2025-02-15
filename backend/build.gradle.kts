f585ed7b2e4195308041bab6206ac1d3c35992a4
Nayeon Kim <inourbubble2@gmail.com>
2025-02-15 19:33:42 +0900

    chore: Add configuration for @WebMvcTest

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.12")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
